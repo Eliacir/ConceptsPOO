318849a85b6e6b0493f8ed4c54600f317892e47a
Eliacir <eliacir14@gmail.com>
2022-06-29 09:36:43 -0500

    Finish day class

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -1,4 +1,18 @@
 ﻿
-Console.WriteLine("============");
+
+using ConceptsPOO;
+
 Console.WriteLine("POO Concepts");
 Console.WriteLine("==============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 2, 7));
+    Console.WriteLine(new Date(2022, 06, 5));
+    Console.WriteLine(new Date(2022, 4, 30));
+   Console.WriteLine(new Date(2024, 2, 29));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
